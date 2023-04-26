5f237405f05ff6792cfa92ddafa46c583631f124
qbuus <jkocz867@gmail.com>
2023-04-26 14:01:29 +0200

    redux auth, authUser, provided to app

---
+++ todofrontend/src/models/User.ts
@@ -0,0 +1,15 @@
+export type User = {
+  username: string;
+  email: string;
+};
+
+export type SignIn = {
+  username: string;
+  password: string;
+};
+
+export type SignUp = {
+  username: string;
+  email: string;
+  password: string;
+};
--- todofrontend/src/redux/reduxState.ts
+++ todofrontend/src/redux/reduxState.ts
@@ -1,24 +1,34 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { User } from "../models/User";
 
 type initialStateBody = {
   isUserAuthenticated: boolean;
+  AuthenticatedUser: User | null;
 };
 
 const initialAppState: initialStateBody = {
   isUserAuthenticated: false,
+  AuthenticatedUser: null,
 };
 
 const appSlice = createSlice({
   name: "app",
   initialState: initialAppState,
-  reducers: {},
+  reducers: {
+    isAuth(state) {
+      state.isUserAuthenticated = !state.isUserAuthenticated;
+    },
+    authUser(state, action) {
+      state.AuthenticatedUser = action.payload;
+    },
+  },
 });
 
 const store = configureStore({
   reducer: appSlice.reducer,
 });
 
-export const {} = appSlice.actions;
+export const { isAuth, authUser } = appSlice.actions;
 
 export default store;
 
