f8a5986a5575e286663500183acb8080a371e86d
qbuus <jkocz867@gmail.com>
2023-04-26 12:44:40 +0200

    MainContent display

--- todofrontend/src/App.tsx
+++ todofrontend/src/App.tsx
@@ -2,15 +2,16 @@ import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import NotFoundPage from "./pages/NotFoundPage";
 import { Container } from "react-bootstrap";
+import MainContent from "./pages/MainContent";
 
 function App() {
   return (
     <BrowserRouter>
       <Container>
         <Routes>
+          <Route path="/" element={<MainContent />} />
           <Route path="/*" element={<NotFoundPage />} />
         </Routes>
-        <div>Start</div>;
       </Container>
     </BrowserRouter>
   );
