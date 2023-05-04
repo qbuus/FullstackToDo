e86dcc12204ed601e7c6df4b221d6056318ca25a
qbuus <jkocz867@gmail.com>
2023-05-04 11:54:58 +0200

    font color change in NotFoundPage

--- todofrontend/src/pages/NotFoundPage.tsx
+++ todofrontend/src/pages/NotFoundPage.tsx
@@ -6,12 +6,12 @@ export default function NotFoundPage() {
       <div className="text-center">
         <h1 className="display-1 fw-bold">404</h1>
         <p className="fs-3">
-          <span className="text-primary">Page not found</span>
+          <span className="text-dark">Page not found</span>
         </p>
         <p className="lead">
           The page you're looking for doesn't exist
         </p>
-        <Link to="/" className="btn btn-primary">
+        <Link to="/" className="btn btn-dark fs-3 px-3 py-2">
           Home
         </Link>
       </div>
