ed6b78f5d0b1e63ce831dde1d3f789fa21ef4847
qbuus <jkocz867@gmail.com>
2023-05-05 12:35:29 +0200

    user creation and signing up limit

--- backend/src/routes/Users.ts
+++ backend/src/routes/Users.ts
@@ -1,11 +1,22 @@
 import express from "express";
 import * as users from "../controllers/users";
 import { requiredAuthentication } from "../middleware/authentication";
+import rateLimit from "express-rate-limit";
+
+const createAccountLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 1,
+  message:
+    "Too many account creation requests. It is limited to 1 request per 1 minute",
+  standardHeaders: true,
+  legacyHeaders: false,
+  statusCode: 429,
+});
 
 const router = express.Router();
 
-router.post("/signup", users.signUp);
-router.post("/signin", users.SignIn);
+router.post("/signup", createAccountLimiter, users.signUp);
+router.post("/signin", createAccountLimiter, users.SignIn);
 router.get(
   "/",
   requiredAuthentication,
