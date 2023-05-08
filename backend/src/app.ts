ad844332030633c762fae4671b505d6a313e0a41
qbuus <jkocz867@gmail.com>
2023-05-08 13:56:14 +0200

    vercel config for backend, moved everything to 1 file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,11 +13,15 @@ import env from "./utils/validateEnv";
 import ToDoRoutes from "./routes/ToDo";
 import UserRoutes from "./routes/Users";
 import { requiredAuthentication } from "./middleware/authentication";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const PORT = env.PORT || 8020;
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
-
 app.use(express.json());
 
 app.use(
@@ -59,5 +63,18 @@ app.use(
     res.status(status).json({ error: errorMessage });
   }
 );
+
+mongoose
+  .connect(env.MONGO)
+  .then(() => {
+    if (!PORT) {
+      return;
+    } else {
+      app.listen(PORT, () => {
+        console.log(`server is running`);
+      });
+    }
+  })
+  .catch(console.error);
 
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,20 @@
-import app from "./app";
-import env from "./utils/validateEnv";
-import mongoose from "mongoose";
+// import app from "./app";
+// import env from "./utils/validateEnv";
+// import mongoose from "mongoose";
 
-const PORT = env.PORT || 8020;
+// const PORT = env.PORT || 8020;
 
-mongoose
-  .connect(env.MONGO)
-  .then(() => {
-    console.log("database is connected");
-    app.listen(PORT, () => {
-      console.log(`server is running on port: ${PORT}`);
-    });
-  })
-  .catch(console.error);
+// mongoose
+//   .connect(env.MONGO)
+//   .then(() => {
+//     if (!PORT) {
+//       return;
+//     } else {
+//       app.listen(PORT, () => {
+//         console.log(`server is running`);
+//       });
+//     }
+//   })
+//   .catch(console.error);
+
+// module.exports = app;
