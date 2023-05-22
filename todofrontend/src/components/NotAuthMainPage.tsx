82d1722ed5acf3de29bb19e1973008b556075ddc
qbuus <jkocz867@gmail.com>
2023-05-22 17:09:18 +0200

    prior login page styles change

--- todofrontend/src/components/NotAuthMainPage.tsx
+++ todofrontend/src/components/NotAuthMainPage.tsx
@@ -1,30 +1,26 @@
-import { Button, Col, Row } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function NotAuthMainPage() {
   return (
     <div className="vh-100 d-flex flex-column justify-content-center align-items-center gap-5">
-      <div>
-        <Row>
-          <Col>
-            <Link to="/signup">
-              <Button variant="light" as="button">
-                <div className="d-flex px-5 text-black px-5 py-3 fs-2">
-                  Sign up
-                </div>
-              </Button>
-            </Link>
-          </Col>
-        </Row>
+      <div className="d-flex justify-content-center align-items-center w-100 py-3 py-3">
+        <div>
+          <Link to="/signin">
+            <Button variant="light" size="lg">
+              <div className="fs-1">Sign in</div>
+            </Button>
+          </Link>
+        </div>
       </div>
-      <div>
-        <Link to="/signin">
-          <Button variant="dark" as="button">
-            <div className="d-flex px-3 text-white px-2">
-              Already have an account
-            </div>
-          </Button>
-        </Link>
+      <div className="d-flex justify-content-center align-items-center">
+        <div>
+          <Link to="/signup">
+            <Button variant="dark" as="button" size="lg">
+              <div className="fs-4">Sign up</div>
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
